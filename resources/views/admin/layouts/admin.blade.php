65a8a6b1cf1115282af3e7b1ed18d66f8d9c0cef
gowthamardhas <gowtham@ardhas.com>
2024-11-27 16:12:43 +0530

    file load form local

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -285,13 +285,11 @@
     <!-- App js-->
     <script src="{{ url('public/assets/js/app.min.js') }}"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"
-        integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"
-        integrity="sha512-6S5LYNn3ZJCIm0f9L6BCerqFlQ4f5MwNKq+EthDXabtaJvg3TuFLhpno9pcm+5Ynm6jdA9xfpQoMz2fcjVMk9g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ public_plugins('sweetalert/sweetalert2.js') }}"></script>
+    <script src="{{ public_plugins('jqueryvalidation/jquery.validate.min.js') }}"></script>
+    <script src="{{ public_plugins('jqueryvalidation/additional-methods.min.js') }}"></script>
+
+
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/4.0.0/js/jasny-bootstrap.min.js"
         integrity="sha512-T+qL8JzVjquTv+yKR64v+58O+GVCe7A68gbJTzFVs76I7iAcgwisXKyOTaeKZaekcHeiG65p48NDqcMmPgnvIA=="
