eaa26abc15cda7bb601f77c38bf3a2a70d1b6233
swethaardhas <swetha.r@ardhas.com>
2024-12-20 12:49:01 +0530

    to be store user_id in emp table

--- app/Http/Controllers/Cron/CronController.php
+++ app/Http/Controllers/Cron/CronController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\DB;
 
 use Carbon\Carbon;
 use App\Models\Master\Worktemp;
@@ -500,17 +500,27 @@ class CronController extends Controller
 
                 $employee = $this->employee->store($emp_temp);
                 $users = $this->user->store($employee);
+                foreach ($users as $user) {
+                    // if (!empty($user) && isset($user->email)) {
+                    //     $empdetails = $this->employee->selectOne($user->employee_id);
+                    //     if (!empty($empdetails)) {
 
-                // foreach ($users as $user) {
-                //     if (!empty($user) && isset($user->email)) {
-                //         $empdetails = $this->employee->selectOne($user->employee_id);
-                //         if (!empty($empdetails)) {
+                    //         $emp = $empdetails->toArray();
+                    //         Mail::to($user->email)->queue(new EmployeeRegisterEmail($emp));
+                    //     }
+                    // }
+                    if (!empty($user) && isset($user['employee_id'])) {
+                        $userID = DB::table('users')
+                            ->select('id')
+                            ->where('employee_id', $user['employee_id'])
+                            ->first();
 
-                //             $emp = $empdetails->toArray();
-                //             Mail::to($user->email)->queue(new EmployeeRegisterEmail($emp));
-                //         }
-                //     }
-                // }
+                        if ($userID && isset($userID->id)) {
+                            $this->employee->updateUserId($user['employee_id'], $userID->id);
+                        }
+                    }
+                }
+
 
                 if (empty($employee)) {
                     $this->emp_temp->updateAllErrorStatus();
@@ -566,7 +576,7 @@ class CronController extends Controller
 
                 if (!empty($data)) {
                     $work = $this->ppestock->store($data);
-                    Session::flash('sucess','Your data has been created Sucessfully');
+                    Session::flash('sucess', 'Your data has been created Sucessfully');
                     return redirect('ppe_stock_inventory/list');
                 } else {
                     return response()->json(['message' => 'No data found in API response.']);
@@ -587,16 +597,16 @@ class CronController extends Controller
 
             $currentTime = Carbon::now()->format('H:i:s');
             $permits = SafetyPermit::where('trash', 'NO')
-            ->where('permit_status', '!=', STATUS_PLANT_HEAD_APPROVED)
-            ->whereDate('date', Carbon::today()) 
-            ->get();
-    
+                ->where('permit_status', '!=', STATUS_PLANT_HEAD_APPROVED)
+                ->whereDate('date', Carbon::today())
+                ->get();
+
             if ($permits->isNotEmpty()) {
                 foreach ($permits as $permit) {
                     $permit->permit_status = STATUS_PERMIT_EXPIRED;
                     $permit->save();
                 }
-    
+
                 Log::info('Expired permits updated successfully.', ['count' => $permits->count()]);
                 return response()->json(['message' => 'Expired permits updated successfully.']);
             } else {
