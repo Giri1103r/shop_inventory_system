fab5ee9a168ffd9194a7d2c7493081550370da47
Giridharan R <giridharan.r@ardhas.com>
2024-12-30 17:05:06 +0530

    Update SafetyPermitController.php

--- app/Http/Controllers/Safetypermit/SafetyPermitController.php
+++ app/Http/Controllers/Safetypermit/SafetyPermitController.php
@@ -1385,6 +1385,7 @@ class SafetyPermitController extends Controller
             $filename = "Safety Permit.pdf";
             return $mpdf->Output($filename, 'D');
         } catch (Exception $ex) {
+            dd($ex);
             report($ex);
         }
     }
