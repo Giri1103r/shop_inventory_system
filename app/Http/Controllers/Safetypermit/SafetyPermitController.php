56b157f7c9ae0a6a9c1c899b6f2f1e42bc322159
Athuilya <154062141+Athuilya13@users.noreply.github.com>
2024-12-30 15:28:27 +0530

    dd

--- app/Http/Controllers/Safetypermit/SafetyPermitController.php
+++ app/Http/Controllers/Safetypermit/SafetyPermitController.php
@@ -573,7 +573,6 @@ class SafetyPermitController extends Controller
             }
             return view('permit.safetypermit.approvereject', $data);
         } catch (Exception $ex) {
-dd($ex);
             report($ex);
             Session::flash('error', 'Something went wrong Please try again after some time');
             return redirect(admin_url('safetypermit/list'));
