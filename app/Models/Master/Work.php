d48df05002c6f2fcb66df6cea285c1fd228e1d2c
Athuilya <154062141+Athuilya13@users.noreply.github.com>
2025-01-28 16:11:53 +0530

    Update Work.php

--- app/Models/Master/Work.php
+++ app/Models/Master/Work.php
@@ -283,7 +283,6 @@ class Work extends Model
     {
 
         $request = request();
-dd($request);
         $update_array = array(
             'emp_id' => $request->emp_id ?? null,
             'emp_name' => $request->emp_name ?? null,
@@ -291,7 +290,7 @@ dd($request);
             'nationality' => $request->nationality ?? null,
             'biometric_code' => $request->biometric_code ?? null,
             'doi' => DBdatetimeformat($request->doi),
-            'exit_date' => DBdatetimeformat($request->exit_date),
+            'exit_date' => DBdatetimeformat($request->exit_date) ?? null,
             'mobile_no' => $request->mobile_no ?? null,
             'company' => decryptId($request->company),
             'location' => decryptId($request->location),
