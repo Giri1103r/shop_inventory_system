b9acb33c48246bc16b5ffe58635da368ba3819b0
swethaardhas <swetha.r@ardhas.com>
2025-02-10 16:27:44 +0530

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -48,7 +48,7 @@ if (!function_exists('DBdateformat')) {
 
     function DBdateformat($date)
     {
-        if ($date == '' || $date == null) {
+        if ($date == '' || $date == null || $date == '1970-01-01') {
             return '';
         }
         return date('Y-m-d', strtotime($date));
