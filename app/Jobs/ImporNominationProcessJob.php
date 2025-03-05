2323a37b32e2590dde0052359071961389ecabe3
Giridharan R <giridharan.r@ardhas.com>
2025-03-05 17:16:09 +0530

    Update ImporNominationProcessJob.php

--- app/Jobs/ImporNominationProcessJob.php
+++ app/Jobs/ImporNominationProcessJob.php
@@ -54,7 +54,7 @@ class ImporNominationProcessJob  implements ShouldQueue
     public function __construct($details)
     {
 
-dd($details);
+dd($details,'123');
         $this->details = $details;
     }
 
