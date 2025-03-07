2e4f4ff3029dda7c7ad116126d9b59f3f90cd90b
Giridharan R <giridharan.r@ardhas.com>
2025-03-07 15:04:02 +0530

    Update SafetyPermitController.php

--- app/Http/Controllers/Safetypermit/SafetyPermitController.php
+++ app/Http/Controllers/Safetypermit/SafetyPermitController.php
@@ -132,12 +132,27 @@ class SafetyPermitController extends Controller
                             <i class="fa-solid fa-check-to-slot text-success"></i>
                         </a>';
                             }
- dd($row->date , date('Y-m-d'), in_array($row->date, [date('Y-m-d'), date('Y-m-d', strtotime('+1 day'))]));
-                            if (in_array($row->date, [date('Y-m-d'), date('Y-m-d', strtotime('+1 day'))])) {
+                            //  dd($row->date , date('Y-m-d'), in_array($row->date, [date('Y-m-d'), date('Y-m-d', strtotime('+1 day'))]));
+                            //                             if (in_array($row->date, [date('Y-m-d'), date('Y-m-d', strtotime('+1 day'))])) {
+                            //                                 if (($row->permit_status == STATUS_PERMIT_EXPIRED)
+                            //                                     && ($row->created_by == Auth::id() || CheckUserRole(ROLE_SUPERADMIN))
+                            //                                 ) {
+                            //                                     $btn .= '<a href="' . admin_url('safetypermit/permitExtension/' . encryptId($row->id)) . '" class="permitExtension" title="' . __('Permit Extension') . '"><i class="fa fa-external-link"></i></a>';
+                            //                                 }
+                            //                             }
+
+
+                            $permitDate = date('Y-m-d', strtotime($row->date));
+                            $today = date('Y-m-d');
+                            $tomorrow = date('Y-m-d', strtotime('+1 day'));
+
+                            if (in_array($permitDate, [$today, $tomorrow])) {
                                 if (($row->permit_status == STATUS_PERMIT_EXPIRED)
                                     && ($row->created_by == Auth::id() || CheckUserRole(ROLE_SUPERADMIN))
                                 ) {
-                                    $btn .= '<a href="' . admin_url('safetypermit/permitExtension/' . encryptId($row->id)) . '" class="permitExtension" title="' . __('Permit Extension') . '"><i class="fa fa-external-link"></i></a>';
+                                    $btn .= '<a href="' . admin_url('safetypermit/permitExtension/' . encryptId($row->id)) . '"
+                 class="permitExtension" title="' . __('Permit Extension') . '">
+                 <i class="fa fa-external-link"></i></a>';
                                 }
                             }
 
@@ -574,12 +589,12 @@ class SafetyPermitController extends Controller
                     'approved_by' => Auth::id(),
                 );
                 $this->statuslog->create($insert_array);
-            }else{
+            } else {
                 $insert_array = array(
                     'permit_type' => 0,
                     'permit_id' => $safetypermit->id,
                     'from_status' => 0,
-                    'to_status' =>$permit_status,
+                    'to_status' => $permit_status,
                     'is_reject' => null,
                     'remarks' => null,
                     'approved_by' => Auth::id(),
