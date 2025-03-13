f2bad13cc9dbb6b7cca2d272c85148fbd1e0006e
Giridharan R <giridharan.r@ardhas.com>
2025-03-13 11:41:10 +0530

    Update LoginController.php

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -49,6 +49,11 @@ class LoginController extends BaseController
                 $user = Auth::user();
                 $success['token'] =  $user->createToken('karam')->accessToken;
                 $success['name'] =  $user->name;
+                $success['user_details'] = [
+                    'employee_id'   => $user->employee_id,
+                    'role'          => $user->role,
+                    'department_id' => $user->department_id,
+                ];
 
 
                 $token = $request->fcm_token;
