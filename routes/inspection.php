3f1f0464e52320d7d00eee29b9ce7ae9558792b3
Sowndar <sowndar.b@ardhas.com>
2025-03-21 16:46:46 +0530

    Update inspection.php

--- routes/inspection.php
+++ routes/inspection.php
@@ -137,10 +137,6 @@ Route::group(['prefix' => 'inspection/gemba-walk/'], function () {
     Route::post('floor-manager/review/submit', [GembaWalkController::class, 'capaReviewSubmit']);
     Route::get('ehs-officer/{id}', [GembaWalkController::class, 'ehsOfficerReview']);
     Route::post('ehs-officer/review/submit', [GembaWalkController::class, 'ehsReviewSubmit']);
-
-
-
-
 });
 Route::group(['prefix' => 'environment/'], function () {
     Route::group(['prefix' => 'ambient-noise/'], function () {
@@ -189,9 +185,19 @@ Route::group(['prefix' => 'safety/'], function () {
         Route::POST('list', [MonthlyEyeWashInspectionController::class, 'Index']);
         Route::GET('add', [MonthlyEyeWashInspectionController::class, 'Add']);
         Route::POST('add/submit', [MonthlyEyeWashInspectionController::class, 'Store']);
-        Route::GET('get/locations',[MonthlyEyeWashInspectionController::class,'GetLocations']);
-        Route::GET('view/{id}',[MonthlyEyeWashInspectionController::class,'View']);
+        Route::GET('get/locations', [MonthlyEyeWashInspectionController::class, 'GetLocations']);
+        Route::GET('view/{id}', [MonthlyEyeWashInspectionController::class, 'View']);
+        Route::GET('verification/{id}/{employee_type}', [MonthlyEyeWashInspectionController::class, 'Approvals']);
+        Route::POST('ehsofficer/verify/submit', [MonthlyEyeWashInspectionController::class, 'EHSOfficerSubmit']);
+        Route::POST('capa/submit', [MonthlyEyeWashInspectionController::class, 'CAPASubmit']);
+        Route::POST('capa/reverify/submit', [MonthlyEyeWashInspectionController::class, 'CAPAVerifySubmit']);
+        Route::POST('level-one/verify/submit', [MonthlyEyeWashInspectionController::class, 'levelOneManagerSubmit']);
+        Route::POST('level-two/verify/submit', [MonthlyEyeWashInspectionController::class, 'levelTwoManagerSubmit']);
+        Route::GET('export/excel', [MonthlyEyeWashInspectionController::class, 'exportExcel']);
+        Route::GET('export/pdf', [MonthlyEyeWashInspectionController::class, 'exportPdf']);
+        Route::GET('exportViewPdf/{id}', [MonthlyEyeWashInspectionController::class, 'exportViewPdf']);
     });
+
     Route::group(['prefix' => 'forklift-inspection/monthly/'], function () {
         Route::get('list', [MonthlyForkLiftInspectionController::class, 'index']);
         Route::post('list', [MonthlyForkLiftInspectionController::class, 'index']);
@@ -238,7 +244,6 @@ Route::group(['prefix' => 'safety/'], function () {
         Route::get('export/pdf', [FireSafetyEquipmentController::class, 'exportPdf']);
         Route::get('exportViewPdf/{id}', [FireSafetyEquipmentController::class, 'exportViewPdf']);
     });
-
 });
 
 
@@ -318,14 +323,14 @@ Route::group(['prefix' => 'fire/'], function () {
         Route::GET('get/department', [HooterInspectionController::class, 'GetDepartment']);
     });
 
-    Route::group(['prefix' => 'monthly-fire-pump-house-inspection/'],function(){
-        Route::GET('list',[MonthlyFirePumpHouseController::class,'Index']);
-        Route::POST('list',[MonthlyFirePumpHouseController::class,'Index']);
-        Route::GET('add',[MonthlyFirePumpHouseController::class,'Add']);
-        Route::POST('add/submit',[MonthlyFirePumpHouseController::class,'Store']);
-        Route::GET('view/{id}',[MonthlyFirePumpHouseController::class,'View']);
-        Route::GET('export/excel',[MonthlyFirePumpHouseController::class,'ExportExcel']);
-        Route::GET('export/pdf',[MonthlyFirePumpHouseController::class,'ExportPDF']);
+    Route::group(['prefix' => 'monthly-fire-pump-house-inspection/'], function () {
+        Route::GET('list', [MonthlyFirePumpHouseController::class, 'Index']);
+        Route::POST('list', [MonthlyFirePumpHouseController::class, 'Index']);
+        Route::GET('add', [MonthlyFirePumpHouseController::class, 'Add']);
+        Route::POST('add/submit', [MonthlyFirePumpHouseController::class, 'Store']);
+        Route::GET('view/{id}', [MonthlyFirePumpHouseController::class, 'View']);
+        Route::GET('export/excel', [MonthlyFirePumpHouseController::class, 'ExportExcel']);
+        Route::GET('export/pdf', [MonthlyFirePumpHouseController::class, 'ExportPDF']);
         Route::GET('verification/{id}/{employee_type}', [MonthlyFirePumpHouseController::class, 'approvals']);
         Route::POST('ehsofficer/verify/submit', [MonthlyFirePumpHouseController::class, 'EHSOfficerSubmit']);
         Route::POST('capa/submit', [MonthlyFirePumpHouseController::class, 'CAPASubmit']);
