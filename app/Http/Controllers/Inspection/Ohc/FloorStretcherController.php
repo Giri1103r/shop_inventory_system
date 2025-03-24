1ba9821d90a47f537ddfcd457cc3dbcd5c145548
JabezNehemiah <jabeznehemiah.l@ardhas.com>
2025-03-24 12:17:11 +0530

    FloorStretcher

---
+++ app/Http/Controllers/Inspection/Ohc/FloorStretcherController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Inspection\Ohc;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FloorStretcherController extends Controller
+{
+    //
+}
