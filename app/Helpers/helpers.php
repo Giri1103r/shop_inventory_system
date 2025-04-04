78cd6ddf5c74e871ffbd1d21b574b6cdf1740e39
JabezNehemiah <jabezjemie21@gmail.com>
2025-04-04 13:22:11 +0530

    Hose Box Inspection Completed

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2579,6 +2579,20 @@ if (!function_exists('getMonth')) {
                     } else {
                         return $name->file_path;
                     }
+
+                case HOSE_BOX_INSPECTION:
+                    $name = FireSignatureUpload::where('emp_id', $userid)->where('inspection_id', $id)->where('type', HOSE_BOX_INSPECTION)
+                        ->where('status', 1)->where('trash', 'NO')->first();
+
+                    if ($name == null) {
+                        $name = User::where('id', $userid)->first();
+                        if ($name == null) {
+                            return null;
+                        }
+                        return $name->signature_upload;
+                    } else {
+                        return $name->file_path;
+                    }
             }
         }
     }
