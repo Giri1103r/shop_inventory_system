da4544ad12416dc83a470e290b7ebb1723f368f5
JabezNehemiah <jabeznehemiah.l@ardhas.com>
2025-04-09 16:50:29 +0530

    Fire Alarm  PDF

--- resources/lang/en/title.php
+++ resources/lang/en/title.php
@@ -6,4 +6,5 @@ return [
     'safety_equipment' => 'List of Fire Safety & Rescue Equipment PN International Pvt Ltd',
     'opd_medicine' => 'Monthly OHC First-Aid Medicine Inspection Checklist PN International Pvt.Ltd',
     'medicine_store' => 'Monthly OHC Store Medicine Inspection Checklist PN International Pvt.Ltd',
+    'fire_alarm' => 'FIRE ALARM INSPECTION CHECKLIST PN INTERNATIONAL PVT. LTD.',
 ];
--- resources/views/inspection/Fire/hooter_inspection/viewPdf.blade.php
+++ resources/views/inspection/Fire/hooter_inspection/viewPdf.blade.php
@@ -208,14 +208,18 @@
             <th colspan="4" style="border: 1px solid black; text-align: left; padding: 8px;">DATE OF INSPECTION :-
                 {{ Displaydateformat($forklift_details->date_of_inspection) }}
             </th>
-            <th colspan="3" style="border: 1px solid black; text-align: left; padding: 8px;">LOCATION :- {{ getLocationName($forklift_details->location) }}</th>
-            <th colspan="2" style="border: 1px solid black; text-align: left; padding: 8px;">SHIFT :- {{ GetShiftName($forklift_details->shift) }}</th>
+            <th colspan="3" style="border: 1px solid black; text-align: left; padding: 8px;">LOCATION :-
+                {{ getLocationName($forklift_details->location) }}</th>
+            <th colspan="2" style="border: 1px solid black; text-align: left; padding: 8px;">SHIFT :-
+                {{ GetShiftName($forklift_details->shift) }}</th>
         </tr>
         <tr style="background-color: #ddd;">
-            <th colspan="4" style="border: 1px solid black; text-align: left; padding: 8px;">NEXT DUE ON :- {{ Displaydateformat($forklift_details->next_due) }}</th>
-            <th colspan="3" style="border: 1px solid black; text-align: left; padding: 8px;">UNIT :- {{ GetUnitName($forklift_details->unit) }} </th>
-            <th colspan="2" style="border: 1px solid black; text-align: left; padding: 8px;">FREQUENCY :- {{ GetFrequencyName($forklift_details->frequency) }}
-            </th>
+            <th colspan="4" style="border: 1px solid black; text-align: left; padding: 8px;">NEXT DUE ON :-
+                {{ Displaydateformat($forklift_details->next_due) }}</th>
+            <th colspan="3" style="border: 1px solid black; text-align: left; padding: 8px;">UNIT :-
+                {{ GetUnitName($forklift_details->unit) }} </th>
+            <th colspan="2" style="border: 1px solid black; text-align: left; padding: 8px;">FREQUENCY :-
+                {{ GetFrequencyName($forklift_details->frequency) }}</th>
         </tr>
 
         <!-- Column Headers -->
@@ -223,11 +227,11 @@
             <th rowspan="2" style="border: 1px solid black; padding: 8px;">SR. NO</th>
             <th rowspan="2" style="border: 1px solid black; padding: 8px;">DEPARTMENT</th>
             <th rowspan="2" style="border: 1px solid black; padding: 8px;">RESOURCE CODE</th>
-            <th rowspan="2" style="border: 1px solid black; padding: 8px;">QUANTITY</th>
-            <th colspan="4" style="border: 1px solid black; padding: 8px;">CHECK ITEMS</th>
+            <th colspan="5" style="border: 1px solid black; padding: 8px;">CHECK ITEMS</th>
             <th rowspan="2" style="border: 1px solid black; padding: 8px;">REMARK</th>
         </tr>
         <tr style="background-color: #ddd;">
+            <th style="border: 1px solid black; padding: 8px;">QUANTITY</th>
             <th style="border: 1px solid black; padding: 8px;">BLINKING LIGHT</th>
             <th style="border: 1px solid black; padding: 8px;">CONNECTION</th>
             <th style="border: 1px solid black; padding: 8px;">AUDIBILITY</th>
@@ -267,17 +271,7 @@
             </tr>
         @endforeach
 
-        {{-- <tr>
-            <td colspan="6"
-                style="border: 1px solid black; text-align: left; padding: 10px; background-color: #f0f0f0;">
-                <strong>OBSERVATION</strong>
-            </td>
-            <td colspan="3"
-                style="border: 1px solid black; text-align: center; padding: 10px; background-color: #f0f0f0;">
-                <img src="{{ admin_url($inspection_image) }}" style="width:110px; height:110px;" alt=""
-                    srcset="">
-            </td>
-        </tr> --}}
+        <!-- Signatures Section -->
         <tr>
             <td colspan="3" style="border: 1px solid black; padding: 6px; text-align: center;">
                 <div class="view_data">
@@ -295,8 +289,7 @@
                     @if (!empty($forklift_details->verified_by))
                         <img src="{{ admin_url($verified_by) }}" alt=""
                             style="max-height: 60px; display: block; margin: 0 auto 5px;">
-                        <p style="margin: 0;">Verified By:- {{ getUsername($forklift_details->verified_by) }}
-                        </p>
+                        <p style="margin: 0;">Verified By:- {{ getUsername($forklift_details->verified_by) }}</p>
                     @else
                         <p style="margin: 0;">Verified By:- Not yet verified</p>
                     @endif
@@ -307,8 +300,7 @@
                     @if (!empty($forklift_details->approved_by))
                         <img src="{{ admin_url($approved_by) }}" alt=""
                             style="max-height: 60px; display: block; margin: 0 auto 5px;">
-                        <p style="margin: 0;">Approved By:- {{ getUsername($forklift_details->approved_by) }}
-                        </p>
+                        <p style="margin: 0;">Approved By:- {{ getUsername($forklift_details->approved_by) }}</p>
                     @else
                         <p style="margin: 0;">Approved By:- Not yet approved</p>
                     @endif
@@ -316,6 +308,7 @@
             </td>
         </tr>
     </table>
+
 
     <br>
 
