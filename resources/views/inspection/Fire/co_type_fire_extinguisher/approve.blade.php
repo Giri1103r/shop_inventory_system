59a3975443da69d4517bb31fc6d81d8afb862efa
sakthi48 <sathyamoorthi.s@ardhas.com>
2025-04-16 14:59:29 +0530

    update

--- resources/views/inspection/Fire/co_type_fire_extinguisher/approve.blade.php
+++ resources/views/inspection/Fire/co_type_fire_extinguisher/approve.blade.php
@@ -138,13 +138,22 @@
                                             <div class="card-header-inner p-2">
                                                 <h4 class="text-white">Co2 Type Fire Extinguisher Inspection Observation</h4>
                                             </div>
-                                            <div class="col-md-12 mb-2">
+                                            {{-- <div class="col-md-12 mb-2">
                                                 <div class="form-group form-input">
                                                     <label class="form-label require">{{ __('inspection.obs') }}</label>
                                                     <div class="view_data">
                                                         {{ $inspection->observation }}
                                                     </div>
 
+                                                </div>
+                                            </div> --}}
+                                            <div class="col-md-4 mb-2">
+                                                <div class="form-group form-input">
+                                                    <label
+                                                        class="form-label require">Observation</label>
+                                                    <div class="view_data">
+                                                        {{ $inspection->observation_needed == '1' ? 'YES' : 'NO' }}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -306,7 +315,8 @@
                                     <hr>
                                 </div>
 
-                                @if ($inspection->inspection_status == WAITING_FOR_EHS_OFFICER_VERIFICATION)
+                                @if ($inspection->inspection_status == WAITING_FOR_EHS_OFFICER_VERIFICATION &&
+                                        (checkUserRole(ROLE_FIRE_ASSOCIATES) || isAdmin()))
                                     <div class="row mt-3">
                                         <div class="card-header-inner">
                                             <h4 class="text-white">{{ __('inspection.ehs_officer_verify') }}</h4>
@@ -579,7 +589,7 @@
                                                     $inspection->id,
                                                     CO_TYPE_FIRE_EXTINGUISHER_INSPECTION,
                                                 );
-                                                
+
                                             @endphp
                                             @if (isset($signature))
                                                 <div class="col-md-4 mb-2">
@@ -659,7 +669,8 @@
                                     $inspection->inspection_status == WAITING_FOR_CAPA_ACTION ||
                                         $inspection->inspection_status == L2_MANAGER_REJECTED ||
                                         $inspection->inspection_status == EHS_OFFICER_REJECTED ||
-                                        $inspection->inspection_status == L1_MANAGER_REJECTED)
+                                        $inspection->inspection_status == L1_MANAGER_REJECTED &&
+                                        (checkUserRole(ROLE_FIRE_ASSOCIATES) || isAdmin()))
                                     <div class="row mt-3">
                                         <div class="card-header-inner">
                                             <h4 class="text-white">{{ __('inspection.capa_action') }}</h4>
@@ -712,7 +723,7 @@
                                     </form>
                                 @endif
 
-                                @if ($inspection->inspection_status == WAITING_FOR_CAPA_VERIFICATION)
+                                @if ($inspection->inspection_status == WAITING_FOR_CAPA_VERIFICATION && (checkUserRole(ROLE_EHS_OFFICER) || isAdmin()))
                                     <form method="POST" id="forklistassessmentAdd"
                                         action="{{ admin_url('fire/fire-extinguisher/co2/capa/reverify/submit') }}"
                                         autocomplete="off" enctype="multipart/form-data">
@@ -762,7 +773,7 @@
                                     </form>
                                 @endif
 
-                                @if ($inspection->inspection_status == WAITING_FOR_L1_VERIFICATION)
+                                @if ($inspection->inspection_status == WAITING_FOR_L1_VERIFICATION && (checkUserRole(ROLE_L1_MANAGER) || isAdmin()))
                                     <form method="POST" id="levelOneManager"
                                         action="{{ admin_url('fire/fire-extinguisher/co2/level-one/verify/submit') }}"
                                         autocomplete="off" enctype="multipart/form-data">
@@ -814,7 +825,7 @@
                                     </form>
                                 @endif
 
-                                @if ($inspection->inspection_status == WAITING_FOR_L2_VERIFICATION)
+                                @if ($inspection->inspection_status == WAITING_FOR_L2_VERIFICATION && (checkUserRole(ROLE_L2_MANAGER) || isAdmin()))
                                     <form method="POST" id="levelTwoManager"
                                         action="{{ admin_url('fire/fire-extinguisher/co2/level-two/verify/submit') }}"
                                         autocomplete="off" enctype="multipart/form-data">
