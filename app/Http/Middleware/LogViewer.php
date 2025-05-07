f1386aca4c0f2aa075ebdccdd82cc1d32b9b7b70
gowthamardhas <gowtham@ardhas.com>
2025-05-07 19:23:27 +0530

    Update LogViewer.php

--- app/Http/Middleware/LogViewer.php
+++ app/Http/Middleware/LogViewer.php
@@ -23,7 +23,7 @@ class LogViewer
     $allowedUserId = env('LOG_VIEWER_CHECK_ID');
 
     if (Auth::id() != $allowedUserId) {
-      dd('test',Auth::id(), $allowedUserId);
+      dd('test',Auth::id(), Auth::user(), $allowedUserId);
       abort(403, 'Unauthorized');
     }
 
