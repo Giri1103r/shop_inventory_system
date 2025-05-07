ec84fb1ba3d146d9413f31d526e6ffd3d61b062a
gowthamardhas <gowtham@ardhas.com>
2025-05-07 19:34:19 +0530

    Update log-viewer.php

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -73,8 +73,7 @@ return [
 
     'middleware' => [
         'web',
-        'islogin',
-        // \App\Http\Middleware\LogViewer::class,
+        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     /*
@@ -87,10 +86,8 @@ return [
     */
 
     'api_middleware' => [
-       'islogin',
-        // \App\Http\Middleware\LogViewer::class,
         \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        //\Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     'api_stateful_domains' => env('LOG_VIEWER_API_STATEFUL_DOMAINS') ? explode(',', env('LOG_VIEWER_API_STATEFUL_DOMAINS')) : null,
