9b9e0ea721dcdbdb54346cd3cb1880a6496f556a
gowthamardhas <gowtham@ardhas.com>
2025-05-07 18:56:12 +0530

    log viewer update

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -87,7 +87,7 @@ return [
 
     'api_middleware' => [
         \App\Http\Middleware\LogViewer::class,
-       // \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
