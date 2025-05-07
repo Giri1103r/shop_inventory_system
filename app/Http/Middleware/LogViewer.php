1c6af30d62c74f0b8ae203776f2f8ffa7c4fd16e
gowthamardhas <gowtham@ardhas.com>
2025-05-07 19:30:33 +0530

    Update LogViewer.php

--- app/Http/Middleware/LogViewer.php
+++ app/Http/Middleware/LogViewer.php
@@ -3,30 +3,22 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LogViewer
 {
-  public function handle(Request $request, Closure $next)
+  public function handle($request,Closure $next)
   {
-    session(['requested_url' => $request->fullUrl()]);
+
 
     if (!Auth::check()) {
-      if (!$request->ajax()) {
+  
         return response()->json(['message' => 'Unauthorized'], 419);
-      }
+     
     }
 
-    $allowedUserId = null;
 
-    $allowedUserId = env('LOG_VIEWER_CHECK_ID');
-
-    if (Auth::id() != $allowedUserId) {
-      dd('test',Auth::id(), Auth::user(), $allowedUserId);
-      abort(403, 'Unauthorized');
-    }
-
+  
     return $next($request);
   }
 }
