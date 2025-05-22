7ff8297ed49aa1935b0e5dd14f3fc9b56f7853b8
jabeznehemiah <jabeznehemiah.l@ardhas.com>
2025-05-22 10:38:30 +0530

    fire extinguisher list

--- app/Http/Controllers/Api/Inspection/Fire/FireExtinguisherController.php
+++ app/Http/Controllers/Api/Inspection/Fire/FireExtinguisherController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\Inspection\Fire;
 
+use Exception;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Inspection\Fire\FireStatusLog;
 use App\Models\Inspection\Fire\FireExtinguisher;
 use App\Models\Inspection\Fire\FireExtinguisherType;
@@ -26,10 +28,36 @@ class FireExtinguisherController extends Controller
 
     public function List(Request $request)
     {
-        
+        if (Auth::check()) {
+            try {
+                $data = $this->inspection->listApi();
+                if (count($data) > 0) {
+                    return response()->json([
+                        'success' => true,
+                        'data' => $data,
+                        'message' => 'Data Retrieved Successfully',
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'success' => false,
+                        'data' => $data,
+                        'message' => 'No Data Found',
+                    ], 200);
+                }
+            } catch (Exception $ex) {
+                report($ex);
+                return $this->sendError(
+                    'Unauthorised.',
+                    ['error' => 'Please try again after sometimes'],
+                    406
+                );
+            }
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised'], 401);
+        }
     }
 
-    public function View(Request $request)
+    public function View(Request $request) 
     {
         
     }
