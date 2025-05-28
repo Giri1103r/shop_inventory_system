e658806b5311d6f6750e073c3dec53b9dd6be316
jabeznehemiah <jabeznehemiah.l@ardhas.com>
2025-05-28 12:40:25 +0530

    updated excel

--- app/Http/Controllers/Inspection/Fire/FireExtinguisherController.php
+++ app/Http/Controllers/Inspection/Fire/FireExtinguisherController.php
@@ -154,7 +154,7 @@ class FireExtinguisherController extends Controller
                             $btn .= '<a href="' . admin_url('fire/fire_extinguisher-inspection/export/excel/' . encryptId($row->inspection_id)) . '" style="margin-right: 5px;" title="Excel"> <i class="fas fa-file-excel" style="color: #1D6F42;" aria-hidden="true"></i></a>';
                             return $btn;
                         })
-                        ->rawColumns(['action', 'created_date', 'created_by', 'status', 'inspection_status', 'date_of_inspection','issue_date'])
+                        ->rawColumns(['action', 'created_date', 'created_by', 'status', 'inspection_status', 'date_of_inspection', 'issue_date'])
                         ->setFilteredRecords($data['filter_records'])
                         ->setTotalRecords($data['total_records'])
                         ->skipPaging()
@@ -707,7 +707,6 @@ class FireExtinguisherController extends Controller
                 $web_link =   admin_url('fire/fire_extinguisher-inspection/verification/' . encryptId($inspection_details->id) . '/capa');
                 $to_status = L2_MANAGER_REJECTED;
                 $users = array_merge([$inspection_details->created_by], [$inspection_details->verified_by], [$inspection_details->l1_manager_verified_by]);
-
             }
 
             $mailsubject = 'Fire Extinguisher Inspection';
@@ -926,21 +925,23 @@ class FireExtinguisherController extends Controller
                     ],
                 ]);
 
-                if (file_exists($prepared_by_signature)) {
-                    $drawing = new Drawing();
-                    $drawing->setName('Signature');
-                    $drawing->setDescription('Prepared By');
-                    $drawing->setPath($prepared_by_signature);
-                    $drawing->setCoordinates("B{$signatureRowStart}");
-                    $drawing->setOffsetX(60);
-                    $drawing->setOffsetY(5);
-                    $drawing->setHeight(40);
-                    $drawing->setWorksheet($sheet);
+                // if (file_exists($prepared_by_signature)) {
+                //     $drawing = new Drawing();
+                //     $drawing->setName('Signature');
+                //     $drawing->setDescription('Prepared By');
+                //     $drawing->setPath($prepared_by_signature);
+                //     $drawing->setCoordinates("B{$signatureRowStart}");
+                //     $drawing->setOffsetX(60);
+                //     $drawing->setOffsetY(5);
+                //     $drawing->setHeight(40);
+                //     $drawing->setWorksheet($sheet);
 
-                    $sheet->setCellValue("A{$signatureRowStart}", "\n\n\nPrepared By:\n" . getUsername($inspection_detail->checked_by));
-                } else {
-                    $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:\nInspection not yet started");
-                }
+                //     $sheet->setCellValue("A{$signatureRowStart}", "\n\n\nPrepared By:\n" . getUsername($inspection_detail->checked_by));
+                // } else {
+                //     $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:\nInspection not yet started");
+                // }
+
+                $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:" . getUsername($inspection_detail->checked_by));
 
                 $sheet->mergeCells("F{$signatureRowStart}:I{$signatureRowStart}");
                 $sheet->getStyle("F{$signatureRowStart}:I{$signatureRowStart}")->applyFromArray([
@@ -952,20 +953,26 @@ class FireExtinguisherController extends Controller
                     ],
                 ]);
 
-                if (file_exists($verified_by_signature)) {
-                    $drawing = new Drawing();
-                    $drawing->setName('Signature');
-                    $drawing->setDescription('Verified By');
-                    $drawing->setPath($verified_by_signature);
-                    $drawing->setCoordinates("H{$signatureRowStart}");
-                    $drawing->setOffsetX(5);
-                    $drawing->setOffsetY(5);
-                    $drawing->setHeight(40);
-                    $drawing->setWorksheet($sheet);
+                // if (file_exists($verified_by_signature)) {
+                //     $drawing = new Drawing();
+                //     $drawing->setName('Signature');
+                //     $drawing->setDescription('Verified By');
+                //     $drawing->setPath($verified_by_signature);
+                //     $drawing->setCoordinates("H{$signatureRowStart}");
+                //     $drawing->setOffsetX(5);
+                //     $drawing->setOffsetY(5);
+                //     $drawing->setHeight(40);
+                //     $drawing->setWorksheet($sheet);
 
-                    $sheet->setCellValue("F{$signatureRowStart}", "\n\n\nVerified By:\n" . getUsername($inspection_detail->verified_by));
+                //     $sheet->setCellValue("F{$signatureRowStart}", "\n\n\nVerified By:\n" . getUsername($inspection_detail->verified_by));
+                // } else {
+                //     $sheet->setCellValue("F{$signatureRowStart}", "Verified By:\nInspection not yet completed");
+                // }
+
+                if ($inspection_detail->verified_by != null) {
+                    $sheet->setCellValue("F{$signatureRowStart}", "Verified By:" . getUsername($inspection_detail->verified_by));
                 } else {
-                    $sheet->setCellValue("F{$signatureRowStart}", "Verified By:\nInspection not yet completed");
+                    $sheet->setCellValue("F{$signatureRowStart}", "Verified By:Inspection not yet completed");
                 }
 
                 $sheet->mergeCells("J{$signatureRowStart}:M{$signatureRowStart}");
@@ -978,22 +985,27 @@ class FireExtinguisherController extends Controller
                     ],
                 ]);
 
-                if (file_exists($approved_by_signature)) {
-                    $drawing = new Drawing();
-                    $drawing->setName('Signature');
-                    $drawing->setDescription('Approved By');
-                    $drawing->setPath($approved_by_signature);
-                    $drawing->setCoordinates("K{$signatureRowStart}");
-                    $drawing->setOffsetX(5);
-                    $drawing->setOffsetY(5);
-                    $drawing->setHeight(40);
-                    $drawing->setWorksheet($sheet);
+                // if (file_exists($approved_by_signature)) {
+                //     $drawing = new Drawing();
+                //     $drawing->setName('Signature');
+                //     $drawing->setDescription('Approved By');
+                //     $drawing->setPath($approved_by_signature);
+                //     $drawing->setCoordinates("K{$signatureRowStart}");
+                //     $drawing->setOffsetX(5);
+                //     $drawing->setOffsetY(5);
+                //     $drawing->setHeight(40);
+                //     $drawing->setWorksheet($sheet);
 
-                    $sheet->setCellValue("J{$signatureRowStart}", "\n\n\nApproved By:\n" . getUsername($inspection_detail->approved_by));
+                //     $sheet->setCellValue("J{$signatureRowStart}", "\n\n\nApproved By:\n" . getUsername($inspection_detail->approved_by));
+                // } else {
+                //     $sheet->setCellValue("J{$signatureRowStart}", "Approved By:\nApproval pending");
+                // }
+
+                if ($inspection_detail->approved_by != null) {
+                    $sheet->setCellValue("J{$signatureRowStart}", "Approved By:" . getUsername($inspection_detail->approved_by));
                 } else {
-                    $sheet->setCellValue("J{$signatureRowStart}", "Approved By:\nApproval pending");
+                    $sheet->setCellValue("J{$signatureRowStart}", "Approved By: Approval pending");
                 }
-
 
                 $row = $signatureRowStart + 6;
             }
@@ -1249,20 +1261,23 @@ class FireExtinguisherController extends Controller
                 'borders'   => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
                 'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER, 'wrapText' => true],
             ]);
-            if (file_exists($prepared_by_signature)) {
-                $drawing = new Drawing();
-                $drawing->setName('Signature');
-                $drawing->setDescription('Prepared By');
-                $drawing->setPath($prepared_by_signature);
-                $drawing->setCoordinates("C{$signatureRowStart}");
-                $drawing->setOffsetX(5);
-                $drawing->setOffsetY(5);
-                $drawing->setHeight(40);
-                $drawing->setWorksheet($sheet);
-                $sheet->setCellValue("A{$signatureRowStart}", "\n\n\nPrepared By:\n" . getUsername($inspection->created_by));
-            } else {
-                $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:\nInspection not yet started");
-            }
+
+            // if (file_exists($prepared_by_signature)) {
+            //     $drawing = new Drawing();
+            //     $drawing->setName('Signature');
+            //     $drawing->setDescription('Prepared By');
+            //     $drawing->setPath($prepared_by_signature);
+            //     $drawing->setCoordinates("C{$signatureRowStart}");
+            //     $drawing->setOffsetX(5);
+            //     $drawing->setOffsetY(5);
+            //     $drawing->setHeight(40);
+            //     $drawing->setWorksheet($sheet);
+            //     $sheet->setCellValue("A{$signatureRowStart}", "\n\n\nPrepared By:\n" . getUsername($inspection->created_by));
+            // } else {
+            //     $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:\nInspection not yet started");
+            // }
+
+            $sheet->setCellValue("A{$signatureRowStart}", "Prepared By:" . getUsername($inspection->created_by));
 
             // Verified By
             $sheet->mergeCells("F{$signatureRowStart}:I{$signatureRowStart}");
@@ -1270,19 +1285,25 @@ class FireExtinguisherController extends Controller
                 'borders'   => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
                 'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER, 'wrapText' => true],
             ]);
-            if (file_exists($verified_by_signature)) {
-                $drawing = new Drawing();
-                $drawing->setName('Signature');
-                $drawing->setDescription('Verified By');
-                $drawing->setPath($verified_by_signature);
-                $drawing->setCoordinates("H{$signatureRowStart}");
-                $drawing->setOffsetX(5);
-                $drawing->setOffsetY(5);
-                $drawing->setHeight(40);
-                $drawing->setWorksheet($sheet);
-                $sheet->setCellValue("F{$signatureRowStart}", "\n\n\nVerified By:\n" . getUsername($inspection->updated_by));
+            // if (file_exists($verified_by_signature)) {
+            //     $drawing = new Drawing();
+            //     $drawing->setName('Signature');
+            //     $drawing->setDescription('Verified By');
+            //     $drawing->setPath($verified_by_signature);
+            //     $drawing->setCoordinates("H{$signatureRowStart}");
+            //     $drawing->setOffsetX(5);
+            //     $drawing->setOffsetY(5);
+            //     $drawing->setHeight(40);
+            //     $drawing->setWorksheet($sheet);
+            //     $sheet->setCellValue("F{$signatureRowStart}", "\n\n\nVerified By:\n" . getUsername($inspection->updated_by));
+            // } else {
+            //     $sheet->setCellValue("F{$signatureRowStart}", "Verified By:\nInspection not yet completed");
+            // }
+
+            if ($inspection->verified_by != null) {
+                $sheet->setCellValue("F{$signatureRowStart}", "Verified By:" . getUsername($inspection->updated_by));
             } else {
-                $sheet->setCellValue("F{$signatureRowStart}", "Verified By:\nInspection not yet completed");
+                $sheet->setCellValue("F{$signatureRowStart}", "Verified By: Inspection not yet completed");
             }
 
             // Approved By
@@ -1291,20 +1312,28 @@ class FireExtinguisherController extends Controller
                 'borders'   => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
                 'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER, 'wrapText' => true],
             ]);
-            if (file_exists($approved_by_signature)) {
-                $drawing = new Drawing();
-                $drawing->setName('Signature');
-                $drawing->setDescription('Approved By');
-                $drawing->setPath($approved_by_signature);
-                $drawing->setCoordinates("L{$signatureRowStart}");
-                $drawing->setOffsetX(5);
-                $drawing->setOffsetY(5);
-                $drawing->setHeight(40);
-                $drawing->setWorksheet($sheet);
-                $sheet->setCellValue("J{$signatureRowStart}", "\n\n\nApproved By:\n" . getUsername($inspection->approved_by));
+
+            // if (file_exists($approved_by_signature)) {
+            //     $drawing = new Drawing();
+            //     $drawing->setName('Signature');
+            //     $drawing->setDescription('Approved By');
+            //     $drawing->setPath($approved_by_signature);
+            //     $drawing->setCoordinates("L{$signatureRowStart}");
+            //     $drawing->setOffsetX(5);
+            //     $drawing->setOffsetY(5);
+            //     $drawing->setHeight(40);
+            //     $drawing->setWorksheet($sheet);
+            //     $sheet->setCellValue("J{$signatureRowStart}", "\n\n\nApproved By:\n" . getUsername($inspection->approved_by));
+            // } else {
+            //     $sheet->setCellValue("J{$signatureRowStart}", "Approved By:\nApproval pending");
+            // }
+
+            if ($inspection->approved_by != null) {
+                $sheet->setCellValue("J{$signatureRowStart}", "Approved By:" . getUsername($inspection->approved_by));
             } else {
-                $sheet->setCellValue("J{$signatureRowStart}", "Approved By:\nApproval pending");
+                $sheet->setCellValue("J{$signatureRowStart}", "Approved By: Approval pending");
             }
+
 
             $writer   = new Xlsx($spreadsheet);
             $fileName = 'Fire Extinguisher Inspection.xlsx';
