7cce4ccc8a6345a752feaf3299eb0beea1a24369
Sowndar <sowndar.b@ardhas.com>
2025-05-28 10:22:12 +0530

    Safety signature update

--- resources/views/inspection/Safety/forklift_inspection_monthly/viewpdf.blade.php
+++ resources/views/inspection/Safety/forklift_inspection_monthly/viewpdf.blade.php
@@ -252,17 +252,17 @@
             @endphp --}}
 
             <!-- Signature Section in a Single Row -->
-            {{-- <tr>
+            <tr>
                 <td colspan="4"
                     style="border: 1px solid black; text-align: center; font-weight: bold; vertical-align: middle;">
-                    <img src="{{ admin_url($creator_signature) }}" alt="Checked By Signature" style="height: 50px;">
+                    {{-- <img src="{{ admin_url($creator_signature) }}" alt="Checked By Signature" style="height: 50px;"> --}}
                     <div>Checked & Prepared By: {{ getUsername($forklift_details->created_by) }}</div>
                 </td>
                 <td colspan="4"
                     style="border: 1px solid black; text-align: center; font-weight: bold; vertical-align: middle;">
                     @if ($forklift_details->verified_by != null)
-                        <img src="{{ admin_url($verifier_signature) }}" alt="Verified By Signature"
-                            style="height: 50px;">
+                        {{-- <img src="{{ admin_url($verifier_signature) }}" alt="Verified By Signature"
+                            style="height: 50px;"> --}}
                         <div>Verified By: {{ getUsername($forklift_details->verified_by) }}</div>
                     @else
                         <p>Inspection has not been Verified Yet</p>
@@ -271,14 +271,14 @@
                 <td colspan="4"
                     style="border: 1px solid black; text-align: center; font-weight: bold; vertical-align: middle;">
                     @if ($forklift_details->approved_by != null)
-                        <img src="{{ admin_url($approver_signature) }}" alt="Approved By Signature"
-                            style="height: 50px;">
+                        {{-- <img src="{{ admin_url($approver_signature) }}" alt="Approved By Signature"
+                            style="height: 50px;"> --}}
                         <div>Approved By: {{ getUsername($forklift_details->approved_by) }}</div>
                     @else
                         <p>Inspection has not been Approved Yet</p>
                     @endif
                 </td>
-            </tr> --}}
+            </tr>
         </tbody>
     </table>
     <br>
