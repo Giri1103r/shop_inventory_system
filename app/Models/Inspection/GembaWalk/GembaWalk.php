ea5b1b63869891cdd545394a685495c21da60cc7
Giridharan R <giridharan.r@ardhas.com>
2025-05-28 12:37:26 +0530

    Update GembaWalk.php

--- app/Models/Inspection/GembaWalk/GembaWalk.php
+++ app/Models/Inspection/GembaWalk/GembaWalk.php
@@ -53,14 +53,14 @@ class GembaWalk extends Model
             ->leftJoin('inspection_gemba_walk_status', 'inspection_gemba_walk_status.id', '=', 'inspection_gemba_walk.gemba_walk_status')
             ->leftJoin('inspection_gemba_walk_checklist', 'inspection_gemba_walk_checklist.gemba_walk_id', '=', 'inspection_gemba_walk.id');
 
-        $org_total =  $query;
-        $org_total_counts = $org_total->count();
+
 
         if (CheckUserRole(ROLE_SUPERADMIN) || CheckUserRole(ROLE_EHS_OFFICER)) {
         } else {
-         $query->whereRaw("FIND_IN_SET(?, inspection_gemba_walk_checklist.responsibility_id)", [Auth::id()]);
-
+            $query->whereRaw("FIND_IN_SET(?, inspection_gemba_walk_checklist.responsibility_id)", [Auth::id()]);
         }
+
+
 
         if ($request->search['value'] != null || $request->search['value'] != '') {
             $search = $request->search['value'];
@@ -105,7 +105,9 @@ class GembaWalk extends Model
             $query->whereBetween('inspection_gemba_walk.created_at', [$startDate, $endDate]);
         }
 
-
+        $org_total =  $query;
+        $org_total_counts = $org_total->count();
+        
         $data_count = $query;
         $total_records = $data_count->count();
 
