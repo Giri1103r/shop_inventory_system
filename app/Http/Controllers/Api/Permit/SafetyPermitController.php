db434b2657a297f097792e90ee1f5823f7ac500e
Giridharan R <giridharan.r@ardhas.com>
2025-06-20 12:13:01 +0530

    Update SafetyPermitController.php

--- app/Http/Controllers/Api/Permit/SafetyPermitController.php
+++ app/Http/Controllers/Api/Permit/SafetyPermitController.php
@@ -87,7 +87,7 @@ class SafetyPermitController extends BaseController
                 $data['id'] = $listdata['id'] ?? '';
                 $data['permit_id'] = $listdata['permit_id'] ?? '';
                 $data['unit_id'] =getUnitname( $listdata['unit_id'] ?? '');
-                $data['date'] = $listdata['date'] ?? '';
+                $data['date'] = Displaydateformat($listdata['date'] ?? '');
                 $data['exact_location_job'] = $listdata['exact_location_job'] ?? '';
                 $data['status_name'] = $listdata['status_name'] ?? '';
                 $data['approved_by'] = getUsername($listdata['approved_by'] ?? '');
